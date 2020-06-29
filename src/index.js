a883c675b5c28aca1b694af401f8925f2b00ec5b
Diane Crame <dcrame@icloud.com>
2020-06-29 15:40:43 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let addToy = false;
+let toysUrl = 'http://localhost:3000/toys'
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -12,4 +13,117 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  document.querySelector(".add-toy-form").addEventListener('submit', onSubmitToy)
+  document.getElementById('toy-collection').addEventListener('click', onSubmitLike)
+  fetchToys(toysUrl);
 });
+
+
+function fetchToys(link) {
+  return fetch(link)
+  .then(resp => resp.json())
+  .then(createToyDiv)
+};
+
+function getToyCollectionDiv(){
+  return document.getElementById('toy-collection');
+};
+
+function createDiv(){
+  let div = document.createElement('div');
+  div.className = "card"
+  return div;
+}
+
+function createName(){
+  return document.createElement('h2');
+}
+
+function createImage(){
+  let img = document.createElement('img');
+  img.className = "toy-avatar"
+  return img;
+}
+
+function createLikeCount(){
+  return document.createElement('p');
+}
+
+function createButton(){
+  let button = document.createElement('button');
+  button.className = "like-btn"
+  button.innerHTML = "Like"
+  return button;
+}
+
+function createToyDiv(json) {
+  json.forEach(obj => {  
+   createToy(obj);
+  });
+};
+
+function onSubmitToy(event){
+  event.preventDefault();
+
+  fetch(toysUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+        Accept: "application/json"
+    },
+    body: JSON.stringify({
+      name: `${event.target.name.value}`, 
+      image: `${event.target.image.value}`,
+      likes: 0
+    })
+  })
+  .then(function(response){
+    return response.json();
+  })
+  .then(createToy)
+};
+
+function createToy(object){
+  let toyDiv = getToyCollectionDiv();
+  let div = createDiv();
+  let name = createName();
+  let img = createImage();
+  let likeCount = createLikeCount();
+  let button = createButton();
+  
+  name.innerHTML = object['name']
+  img.src = object['image']
+  likeCount.innerText = object['likes']
+
+  div.appendChild(name);
+  div.appendChild(img);
+  div.appendChild(likeCount);
+  div.appendChild(button);
+  toyDiv.appendChild(div);
+}
+
+function onSubmitLike(event){
+  let likeButtonIsPressed = event.target.className === "like-btn"
+
+
+  if (likeButtonIsPressed) {
+    let like = event.target.previousElementSibling
+    let likeCount = parseInt(like.innerText)
+    like.innerText = `${++likeCount} likes`
+  
+  fetch(toysUrl,{
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify ({
+      likes: like
+    })
+  })
+  .then(function(response){
+    return response.json()
+  })
+  .then(console.log)
+  }
+};
