5501df4c25df5730a740b32461929424e697d5df
nisticism <nistacular@gmail.com>
2020-07-09 22:28:53 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,50 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function collectToys() {
+  return fetch('http://localhost:3000/toys', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        toy_data
+      })
+    })
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(object) {
+      for (const data of object.toys) {
+
+      toy = document.createElement("div");
+      toy.class = "card";
+
+      
+      let name = document.createElement("h2");
+      name.innerText = data["name"]
+      toy.appendChild(name);
+
+      let image = document.createElement("img");
+      image.class = "toy-avatar"
+      image.src = data["toy-image-url"]
+      toy.appendChild(image)
+
+      let likes = document.createElement("p");
+      toy.appendChild(likes)
+
+      let button = document.createElement("button");
+      button.class = "like-btn";
+      toy.appendChild(button);
+
+      document.querySelector("div#toy-collection").appendChild(toy);
+
+      }
+
+    })
+    .catch(function(error) {
+      document.body.innerHTML = error.message
+    })
+}
